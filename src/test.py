904c89ea2ba492a1b4940c54c1f5d9e9599298a0
Brandon.Cooper <cooper.brandon@outlook.com>
2021-11-06 00:20:22 -0400

    Delete test.py

--- src/test.py
+++
@@ -1,9 +0,0 @@
-from OddsJamClient import OddsJamClient;
-from Models import Game;
-from Enum import SportsEnum;
-import pandas as pd;
-from pandas.io.json import json_normalize;
-Client = OddsJamClient('e7803d61-d16a-4e1d-b3b8-406ece9b6483')
-response = Client.GetLeagues();
-print(pd.DataFrame(response.Leagues))
-# print(response.flatten_json())
